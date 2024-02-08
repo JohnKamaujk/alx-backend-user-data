d17d0aa20bd40538a44f6154e68eb94f131474e9
JohnKamaujk <johnjkkamau@gmail.com>
2024-02-08 16:33:19 +0300

    :sparkles:feat:REdactingFormatter Class

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """A module for filtering logs.
 """
+import logging
 import re
 from typing import List
 
@@ -12,3 +13,21 @@ def filter_datum(fields: List[str],
                                                      separator)
     pattern_replace = r'\g<field>={}'.format(redaction)
     return re.sub(pattern_extract, pattern_replace, message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Formats a single record"""
+        message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION,
+                            message, self.SEPARATOR)
