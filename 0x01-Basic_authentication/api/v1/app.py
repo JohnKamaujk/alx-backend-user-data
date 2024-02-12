fa5c54816fcdbd997382e491b1c935202e4fccc7
JohnKamaujk <johnjkkamau@gmail.com>
2024-02-12 16:02:36 +0300

    :sparkles:feat:Basic Authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -9,7 +9,7 @@ import os
 
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
-
+from api.v1.auth.basic_auth import BasicAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -18,6 +18,8 @@ auth = None
 auth_type = getenv('AUTH_TYPE', 'auth')
 if auth_type == 'auth':
     auth = Auth()
+if auth_type == 'basic_auth':
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
