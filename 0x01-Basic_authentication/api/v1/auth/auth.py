b91fe639d7f46f88f959658ba6258486993c4ca3
JohnKamaujk <johnjkkamau@gmail.com>
2024-02-12 13:29:01 +0300

    :sparkles:feat:require auth path

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,16 +2,29 @@
 """Authentication module for the API.
 """
 from flask import request
+import re
 from typing import List
 
 
 class Auth:
     """Authentication class.
     """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Checks if path requires authentication.
+        """Checks if a path requires authentication.
         """
-        return False
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> None:
         """Gets the authorization header field from the request.
