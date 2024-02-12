4bbbf72776ccffac1a7102a1233d498dce289251
JohnKamaujk <johnjkkamau@gmail.com>
2024-02-12 21:07:21 +0300

    :sparkles:feat:Extract Auth Header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,10 +3,22 @@
 """
 
 
+import re
 from .auth import Auth
 
 
 class BasicAuth(Auth):
     """Basic authentication class.
     """
-    pass
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
+        """Extracts the Base64 part of the Authorization header
+        for Basic Authentication.
+        """
+        if type(authorization_header) == str:
+            pattern = r'Basic (?P<token>.+)'
+            field_match = re.fullmatch(pattern, authorization_header.strip())
+            if field_match is not None:
+                return field_match.group('token')
+        return None
