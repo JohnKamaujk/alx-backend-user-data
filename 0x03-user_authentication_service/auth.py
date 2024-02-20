a4184d8f063e536c23956341a661e4fc8d507b07
JohnKamaujk <johnjkkamau@gmail.com>
2024-02-20 16:22:38 +0300

    :sparkles:feat:Destroy session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -70,3 +70,8 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroys a given User session
+        """
+        self._db.update_user(user_id, session_id=None)
