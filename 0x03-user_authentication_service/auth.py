16733f4d757d67b5b03289404c8d6234d1b84223
JohnKamaujk <johnjkkamau@gmail.com>
2024-02-20 16:15:02 +0300

    :sparkles:feat:Retrieve User by session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -61,3 +61,12 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> str:
+        """Retrieves User with given session id
+        """
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
