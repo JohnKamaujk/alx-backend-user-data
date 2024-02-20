7c7b6ec246e791852a4f3288439caa27deac1c6c
JohnKamaujk <johnjkkamau@gmail.com>
2024-02-20 16:08:32 +0300

    :sparkles:feat:Login feature

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Flask app with authentication features.
 """
-from flask import Flask, jsonify, request
+from flask import Flask, abort, jsonify, request
 
 from auth import Auth
 
@@ -30,6 +30,24 @@ def users() -> str:
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login() -> str:
+    """Create session for a user
+    Return:
+        - The user created payload.
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if (AUTH.valid_login(email, password)):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
