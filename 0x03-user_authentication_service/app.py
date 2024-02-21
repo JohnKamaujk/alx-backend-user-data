ecaa102444eaa63455710a7d2f0c7ca9a78ad156
JohnKamaujk <johnjkkamau@gmail.com>
2024-02-21 14:59:07 +0300

    :sparkles:feat:Update user pasword route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -81,7 +81,7 @@ def profile() -> str:
 
 @app.route("/reset_password", methods=["POST"], strict_slashes=False)
 def get_reset_password_token() -> str:
-    """Retrieves a user's reset pass token
+    """Creates a user's reset pass token
     Return:
         - user's email and reset token as payload
     """
@@ -89,6 +89,23 @@ def get_reset_password_token() -> str:
     try:
         reset_token = AUTH.get_reset_password_token(email)
         return jsonify({"email": email, "reset_token": reset_token})
+    except ValueError:
+        abort(403)
+
+
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
+def update() -> str:
+    """Updates a user's password
+    Return:
+        - user's email and messsage as payload
+    """
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"})
     except ValueError:
         abort(403)
 
